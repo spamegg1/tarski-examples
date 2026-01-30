5b748c649b8c239b89eac25ee38edef7ffeb97ec
spamegg <spamegg@pm.me>
2026-01-30 13:01:01 +0300

    improve example 10

--- 10/10.scala
+++ 10/10.scala
@@ -6,7 +6,7 @@ val SherlockWorld: Grid = Map(
   (3, 5) -> Block(Sml, Sqr, Blu),
   (4, 3) -> Block(Sml, Sqr, Red),
   (7, 1) -> Block(Sml, Tri, Red),
-  (7, 3) -> Block(Sml, Tri, Lim)
+  (7, 3) -> Block(Big, Tri, Lim)
 )
 
 val SherlockSentences = Seq(
@@ -14,9 +14,12 @@ val SherlockSentences = Seq(
   fof"Cir(b) ↔ Cir(c)",
   fof"Sqr(b) ↔ Sqr(c)",
   fof"Tri(a) ∧ ¬Tri(b)",
+  fof"¬Tone(a, b) ∧ ¬Tone(c, b)",
   fof"Bel(a, b) → (Bel(b, c) ∨ Bel(c, b))",
   fof"Left(a, c) → ¬Left(a, b)",
-  fof"Abv(b, a) ↔ Abv(c, b)"
+  fof"Abv(b, a) ↔ Abv(c, b)",
+  fof"More(a, b) ↔ Size(b, c)",
+  fof"Col(a, b) → ¬Row(b, c)"
 )
 
 @main
