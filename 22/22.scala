0ec1987f7bf12ff1b8bf1f2793c08a77a813af48
spamegg <spamegg@pm.me>
2026-02-02 19:19:48 +0300

    fix ex 22

--- 22/22.scala
+++ 22/22.scala
@@ -2,13 +2,13 @@ import tarski.main.*, Sizes.*, Shape.*, Tone.*
 
 val CNFSentences = Seq(
   fof"(Left(a, b) ∨ Abv(a, b)) ∧ Sqr(a)",
-  fof"(Left(a, b) ∧ Sqr(a)) ∨ (Abv(a, b) ∧ Sqr(a))",
+  // add disjunctive normal form here!
   fof"More(a, b) ∧ (Sqr(a) ∨ Tri(a) ∨ a = b)",
-  fof"(More(a, b) ∧ Sqr(a)) ∨ (More(a, b) ∧ Tri(a)) ∨ (More(a, b) ∧ a = b)",
+  // add disjunctive normal form here!
   fof"(Btw(a, b, c) ∨ Tri(a) ∨ ¬Tri(b)) ∧ Cir(c)",
-  fof"(Btw(a, b, c) ∧ Cir(c)) ∨ (Tri(a) ∧ Cir(c)) ∨ (¬Tri(b) ∧ Cir(c))",
-  fof"Sqr(a) ∧ Sqr(b) ∧ (¬Sml(a) ∨ ¬Sml(b))",
-  fof"(Sqr(a) ∧ Sqr(b) ∧ ¬Sml(a)) ∨ (Sqr(a) ∧ Sqr(b) ∧ ¬Sml(b))"
+  // add disjunctive normal form here!
+  fof"Sqr(a) ∧ Sqr(b) ∧ (¬Sml(a) ∨ ¬Sml(b))"
+  // add disjunctive normal form here!
 )
 
 // Here are a few worlds to test your DNF sentences""
