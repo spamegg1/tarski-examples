24769749c0f242655cd8952e06393298e429d51c
spamegg <spamegg@pm.me>
2026-02-24 21:06:44 +0300

    ex q19

--- examples/quantifier-logic/19/q19.scala
+++ examples/quantifier-logic/19/q19.scala
@@ -1,11 +1,25 @@
-import tarski.main.*, Sizes.*, Shape.*, Tone.*
+import tarski.main.*
 
-val worldQ19: Grid = Map(
-)
-
-val sentencesQ19 = Seq(
-  fof"" // ¬∧∨→↔∀∃
+val FregeSentences = Seq(
+  fof"∃x ∃y Left(x, y)",
+  fof"∃x ∃y (Tri(x) ∧ Sqr(y))",
+  fof"∃x ∃y (Tri(x) ∧ Left(x, y))",
+  fof"∃x ∃y (Tri(x) ∧ Left(x, y) ∧ Sqr(y))",
+  fof"∃x ∃y (Mid(x) ∧ Tri(y))",
+  fof"∃x ∃y (Mid(x) ∧ More(y, x))",
+  fof"∃x ∃y (Mid(x) ∧ Tri(y) ∧ More(y, x))",
+  fof"∀x ∀y Left(x, y)",
+  fof"∀x ∀y (Left(x, y) → Sqr(x))",
+  fof"∀x ∀y ((Sqr(x) ∧ Tri(y)) → Left(x, y))",
+  fof"∀x ∀y ((Big(x) ∧ Sml(y)) → Left(x, y))",
+  fof"∀x ∀y More(x, y)",
+  fof"∀x ∀y (More(x, y) → Sqr(x))",
+  fof"∀x ∀y ((Sqr(x) ∧ Tri(y)) → More(x, y))"
 )
 
 @main
-def runQ19 = runWorld(worldQ19, sentencesQ19)
+def runQ19 = runWorld(PeirceWorld, FregeSentences)
+
+// Play the game if you are stuck.
+@main
+def playQ19 = playGame(PeirceWorld, ???) // add sentence here
--- examples/quantifier-logic/19/solution.md
+++ examples/quantifier-logic/19/solution.md
@@ -1,1 +1,9 @@
 # 19 - solution
+
+Initial evaluation:
+
+![q19a](q19a.png)
+
+Moving and resizing a single block:
+
+![q19b](q19b.png)
